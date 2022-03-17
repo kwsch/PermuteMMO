bc99c2f951c4b4d06545ab42cb7e30486ee9f39e
Kurt <kaphotics@gmail.com>
2022-03-16 23:10:22 -0700

    Add util for automation (timid indicator)

--- PermuteMMO.Lib/Permutation/Advance.cs
+++ PermuteMMO.Lib/Permutation/Advance.cs
@@ -40,4 +40,30 @@ public static class AdvanceExtensions
         Advance.G3 => "~3[1]",
         _ => throw new ArgumentOutOfRangeException(nameof(advance), advance, null)
     };
+
+    /// <summary>
+    /// Gets the count of advances required.
+    /// </summary>
+    public static int AdvanceCount(this Advance advance) => advance switch
+    {
+        Advance.A1 => 1,
+        Advance.A2 => 2,
+        Advance.A3 => 3,
+        Advance.A4 => 4,
+
+        Advance.G1 => 1,
+        Advance.G2 => 2,
+        Advance.G3 => 3,
+        _ => 0,
+    };
+
+    /// <summary>
+    /// Indicates if a multi-battle is required for this advancement.
+    /// </summary>
+    public static bool IsMulti(this Advance advance) => advance.AdvanceCount() > 1;
+
+    /// <summary>
+    /// Indicates if any advance requires a multi-battle for advancement.
+    /// </summary>
+    public static bool IsAnyMulti(this IEnumerable<Advance> advances) => advances.Any(z => z.IsMulti());
 }
