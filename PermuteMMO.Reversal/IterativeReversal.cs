33bbe00f57d5dbe19f40584f1d4fcf34e3efad60
Kurt <kaphotics@gmail.com>
2022-03-27 13:59:08 -0700

    Initial dll import attempt

---
+++ PermuteMMO.Reversal/IterativeReversal.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using PKHeX.Core;
+
+namespace PermuteMMO.Reversal;
+
+public static class IterativeReversal
+{
+    private const string LibraryPath = "PLA-SeedFinder";
+
+    static IterativeReversal()
+    {
+        const string dllPath = LibraryPath + ".dll";
+        if (!File.Exists(dllPath))
+            File.WriteAllBytes(dllPath, Properties.Resources.PLA_SeedFinder);
+    }
+
+    [DllImport(LibraryPath, CallingConvention = CallingConvention.Cdecl)]
+    private static extern int pa_PLA_find_seeds(uint pid, uint ec, ref int ivs, byte max_rolls, ref ulong seeds, ref byte rolls, int length);
+
+    /// <summary>
+    /// Finds all seeds using the Pokémon Automation's reversal algorithm (parallelized brute forcing)
+    /// </summary>
+    /// <param name="pid">Entity PID</param>
+    /// <param name="ec">Entity Encryption Constant</param>
+    /// <param name="ivs">Entity IVs</param>
+    /// <param name="max_rolls">Maximum amount of shiny rolls permitted</param>
+    /// <param name="seeds">Result storage for all seeds</param>
+    /// <param name="rolls">Result storage for amount of shiny rolls performed prior to the seed attempt stopping</param>
+    /// <returns>Count of seed-rolls stored in the input spans.</returns>
+    public static int FindSeeds(uint pid, uint ec, ReadOnlySpan<int> ivs, byte max_rolls, Span<ulong> seeds, Span<byte> rolls)
+    {
+        Debug.Assert(seeds.Length == rolls.Length);
+        var length = seeds.Length;
+
+        var ptrIVs = MemoryMarshal.GetReference(ivs);
+        var ptrSeeds = MemoryMarshal.GetReference(seeds);
+        var ptrRoll = MemoryMarshal.GetReference(rolls);
+        return pa_PLA_find_seeds(pid, ec, ref ptrIVs, max_rolls, ref ptrSeeds, ref ptrRoll, length);
+    }
+
+    /// <inheritdoc cref="FindSeeds(uint,uint,ReadOnlySpan{int},byte,Span{ulong},Span{byte})"/>
+    public static int FindSeeds(PKM pk, byte max_rolls, Span<ulong> seeds, Span<byte> rolls)
+    {
+        return FindSeeds(pk.PID, pk.EncryptionConstant, pk.IVs, max_rolls, seeds, rolls);
+    }
+
+    public static ulong[] GetSeeds(uint pid, uint ec, Span<int> ivs, byte max_rolls)
+    {
+        const int overkill = 0x10; // normally 0-2 results, but let's go overboard :)
+        Span<ulong> possible = stackalloc ulong[overkill];
+        Span<byte> rolls = stackalloc byte[overkill];
+
+        int count = FindSeeds(pid, ec, ivs, max_rolls, possible, rolls);
+        Debug.Assert(count <= overkill);
+
+        return possible[..count].ToArray();
+    }
+
+    public static ulong[] GetSeeds(PKM pk, byte max_rolls)
+    {
+        const int overkill = 0x10; // normally 0-2 results, but let's go overboard :)
+        Span<ulong> possible = stackalloc ulong[overkill];
+        Span<byte> rolls = stackalloc byte[overkill];
+
+        int count = FindSeeds(pk, max_rolls, possible, rolls);
+        Debug.Assert(count <= overkill);
+
+        return possible[..count].ToArray();
+    }
+}
---
+++ PermuteMMO.Tests/ReversalTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using PermuteMMO.Reversal;
+using PKHeX.Core;
+using Xunit;
+
+namespace PermuteMMO.Tests;
+
+public class ReversalTests
+{
+    [Fact]
+    public void TestReversal()
+    {
+        var pk1 = Properties.Resources.Tentacool1;
+        var pk2 = Properties.Resources.Tentacool2;
+        var pk3 = Properties.Resources.Tentacool3;
+        var pk4 = Properties.Resources.Tentacool4;
+
+        var all = new[] { pk1, pk2, pk3, pk4 };
+        foreach (var d in all)
+        {
+            var pa8 = new PA8(d);
+            var seeds = IterativeReversal.GetSeeds(pa8, 32);
+            seeds.Should().NotBeEmpty();
+        }
+    }
+}
