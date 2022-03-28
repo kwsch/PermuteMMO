7b0815b166acc5a7ab3a142959acd5c2f39d0c30
Kurt <kaphotics@gmail.com>
2022-03-27 21:04:47 -0700

    Update Program.cs

--- PermuteMMO.ReversalApp/Program.cs
+++ PermuteMMO.ReversalApp/Program.cs
@@ -5,12 +5,13 @@ var result = GroupSeedFinder.FindSeeds(entityFolderName).FirstOrDefault();
 if (result is default(ulong))
 {
     Console.WriteLine("No group seeds found with the input data. Double check your inputs.");
-    return;
 }
-
-// Print seeds
-Console.WriteLine("Found seed!");
-Console.WriteLine(result);
+else
+{
+    Console.WriteLine("Found seed!");
+    Console.WriteLine(result);
+}
 
 Console.WriteLine();
 Console.WriteLine("Press [ENTER] to exit.");
+Console.ReadLine();
