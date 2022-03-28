ef175184a8cef9c4db3e63c9a6e23ff0e2dc09a3
Lusamine <30205550+Lusamine@users.noreply.github.com>
2022-03-27 19:25:05 -0500

    Clean up extra seeds in z3

--- PermuteMMO.Reversal/GenSeedReversal.cs
+++ PermuteMMO.Reversal/GenSeedReversal.cs
@@ -31,9 +31,9 @@ public static class GenSeedReversal
         }
     }
 
-    public static IList<ulong> GetAllGroupSeeds(ulong seed1, ulong seed2, ulong seed3, ulong seed4)
+    public static IList<ulong> GetAllGroupSeeds(ulong seed)
     {
-        var seeds = FindPotentialGroupSeeds(seed1, seed2, seed3, seed4);
+        var seeds = FindPotentialGroupSeeds(seed);
 
         var result = new List<ulong>();
         foreach (var _seed in seeds)
@@ -41,11 +41,11 @@ public static class GenSeedReversal
         return result;
     }
 
-    public static IEnumerable<ulong> FindPotentialGroupSeeds(ulong seed1, ulong seed2, ulong seed3, ulong seed4)
+    public static IEnumerable<ulong> FindPotentialGroupSeeds(ulong seed)
     {
         using var ctx = new Context(new Dictionary<string, string> { { "model", "true" } });
 
-        var exp = CreateGroupSeedModel(ctx, seed1, seed2, seed3, seed4, out var s0);
+        var exp = CreateGroupSeedModel(ctx, seed, out var s0);
 
         while (Check(ctx, exp) is { } m)
         {
@@ -93,29 +93,16 @@ public static class GenSeedReversal
         return solver.Model;
     }
 
-    private static BoolExpr CreateGroupSeedModel(Context ctx, ulong seed1, ulong seed2, ulong seed3, ulong seed4, out BitVecExpr s0)
+    private static BoolExpr CreateGroupSeedModel(Context ctx, ulong seed, out BitVecExpr s0)
     {
         s0 = ctx.MkBVConst("s0", 64);
         BitVecExpr s1 = ctx.MkBV(Xoroshiro128Plus.XOROSHIRO_CONST, 64);
 
-        var real_seed1 = ctx.MkBV(seed1, 64);
-        var real_seed2 = ctx.MkBV(seed2, 64);
-        var real_seed3 = ctx.MkBV(seed3, 64);
-        var real_seed4 = ctx.MkBV(seed4, 64);
+        var real_seed = ctx.MkBV(seed, 64);
 
-        var genseed_check1 = AdvanceSymbolicNext(ctx, ref s0, ref s1); // genseed1
-        AdvanceSymbolicNext(ctx, ref s0, ref s1); // unknown
-        var genseed_check2 = AdvanceSymbolicNext(ctx, ref s0, ref s1); // genseed2
-        AdvanceSymbolicNext(ctx, ref s0, ref s1); // unknown
-        var genseed_check3 = AdvanceSymbolicNext(ctx, ref s0, ref s1); // genseed3
-        AdvanceSymbolicNext(ctx, ref s0, ref s1); // unknown
-        var genseed_check4 = AdvanceSymbolicNext(ctx, ref s0, ref s1); // genseed4
-        AdvanceSymbolicNext(ctx, ref s0, ref s1); // unknown
+        var genseed_check = AdvanceSymbolicNext(ctx, ref s0, ref s1);
 
-        var exp1 = ctx.MkEq(real_seed1, genseed_check1);
-        var exp2 = ctx.MkEq(real_seed2, genseed_check2);
-        var exp3 = ctx.MkEq(real_seed3, genseed_check3);
-        var exp4 = ctx.MkEq(real_seed4, genseed_check4);
-        return ctx.MkAnd(exp1, exp2, exp3, exp4);
+        var exp = ctx.MkEq(real_seed, genseed_check);
+        return ctx.MkAnd(exp);
     }
 }
